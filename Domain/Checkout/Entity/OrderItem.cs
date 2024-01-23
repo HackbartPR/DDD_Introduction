a0fcbf3955cc1db7fcab6b2b974a10f2d424d557
HackbartPR <cgharckbart@gmail.com>
2024-01-23 07:48:29 -0300

    feature: Add OrderItem

---
+++ Domain/Checkout/Entity/OrderItem.cs
@@ -0,0 +1,56 @@
+﻿using Domain._Shared.Entity;
+
+namespace Domain.Checkout.Entity
+{
+    public class OrderItem : BaseEntity
+    {
+        public Guid Id { get; private set; }
+        public long Price { get; private set; }
+        public long Total { get; private set; }
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+        public Guid ProductId { get; private set; }
+
+        public OrderItem(Guid id, string name, int quantity, long price, Guid productId)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            ProductId = productId;
+            Total = CalculateTotal();
+
+            Validate();
+        }
+
+        public override void Validate()
+        {
+            if (Id == Guid.Empty) throw new ArgumentException("Id is required!");
+            if (string.IsNullOrEmpty(Name)) throw new ArgumentException("Name is required!");
+            if (Quantity <= 0) throw new ArgumentException("Quantity shoud be a positive number");
+            if (Price < 0) throw new ArgumentException("Quantity shoud be a positive number");
+            if (ProductId == Guid.Empty) throw new ArgumentException("ProductId is required!");
+        }
+
+        public void ChangePrice(long price) 
+        { 
+            Price = price;
+            Total = CalculateTotal();
+
+            Validate();
+        }
+
+        public void ChangeQuantity(int  quantity)
+        {
+            Quantity = quantity;
+            Total = CalculateTotal();
+
+            Validate();
+        }
+
+        private long CalculateTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
---
+++ DomainUT/Checkout/Entity/OrderItemUT.cs
@@ -0,0 +1,219 @@
+﻿using Domain.Checkout.Entity;
+
+namespace DomainUT.Checkout.Entity
+{
+    public class OrderItemUT
+    {
+        [Fact]
+        public void Create_OrderItem_OnSuccess()
+        {
+            // Arrange
+            long price = 10;
+            int quantity = 2;
+            string name = "Teste";
+            Guid id = Guid.NewGuid();
+            long total = quantity * price;
+
+            // Act
+            OrderItem product = new(id, name, quantity, price, Guid.NewGuid());
+
+            // Assert
+            Assert.True(product.Id == id);
+            Assert.True(product.Name == name);
+            Assert.True(product.Price == price);
+            Assert.True(product.Total == total);
+            Assert.True(product.Quantity == quantity);
+        }
+
+        [Fact]
+        public void Create_OrderItem_InvalidId_OnFailed()
+        {
+            // Arrange
+            long price = 10;
+            int quantity = 2;
+            Guid id = Guid.Empty;
+            string name = "Teste";
+            long total = quantity * price;
+            Guid productId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, name, quantity, price, productId));
+        }
+
+        [Fact]
+        public void Create_OrderItem_InvalidName_OnFailed()
+        {
+            // Arrange
+            long price = 10;
+            int quantity = 2;
+            Guid id = Guid.NewGuid();
+            string name = string.Empty;
+            long total = quantity * price;
+            Guid productId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, name, quantity, price, productId));
+        }
+
+        [Fact]
+        public void Create_OrderItem_Price_OnFailed()
+        {
+            // Arrange
+            long price = -10;
+            int quantity = 2;
+            Guid id = Guid.NewGuid();
+            string name = "Teste";
+            long total = quantity * price;
+            Guid productId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, name, quantity, price, productId));
+        }
+
+        [Fact]
+        public void Create_OrderItem_Quantity_OnFailed()
+        {
+            // Arrange
+            long price = 10;
+            int quantity = -2;
+            Guid id = Guid.NewGuid();
+            string name = "Teste";
+            long total = quantity * price;
+            Guid productId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, name, quantity, price, productId));
+        }
+
+        [Fact]
+        public void Create_OrderItem_ProductId_OnFailed()
+        {
+            // Arrange
+            long price = 10;
+            int quantity = 2;
+            Guid id = Guid.NewGuid();
+            string name = "Teste";
+            long total = quantity * price;
+            Guid productId = Guid.Empty;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, name, quantity, price, productId));
+        }
+
+        [Fact]
+        public void ChangePrice_OnSuccess()
+        {
+            // Arrange
+            string name = "Teste";
+            Guid id = Guid.NewGuid();
+
+            int price = 100;
+            int newPrice = 200;
+
+            OrderItem item = new(id, name, 2, price, Guid.NewGuid());
+
+            // Act
+            item.ChangePrice(newPrice);
+
+            // Assert
+            Assert.True(item.Price == newPrice);
+        }
+
+        [Fact]
+        public void ChangePrice_OnFailed()
+        {
+            // Arrange
+            string name = "Teste";
+            Guid id = Guid.NewGuid();
+
+            int price = 100;
+            int newPrice = -200;
+
+            OrderItem item = new(id, name, 2, price, Guid.NewGuid());
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => item.ChangePrice(newPrice));
+        }
+
+        [Fact]
+        public void ChangeQuantity_OnSuccess()
+        {
+            // Arrange
+            string name = "Teste";
+            Guid id = Guid.NewGuid();
+
+            int quantity = 2;
+            int newQuant = 3;
+
+            OrderItem item = new(id, name, quantity, 10, Guid.NewGuid());
+
+            // Act
+            item.ChangeQuantity(newQuant);
+
+            // Assert
+            Assert.True(item.Quantity == newQuant);
+        }
+
+        [Fact]
+        public void ChangeQuantity_OnFailed()
+        {
+            // Arrange
+            string name = "Teste";
+            Guid id = Guid.NewGuid();
+
+            int quantity = 2;
+            int newQuant = -3;
+
+            OrderItem item = new(id, name, quantity, 10, Guid.NewGuid());
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => item.ChangeQuantity(newQuant));
+        }
+
+        [Fact]
+        public void CalculateTotal_ChangingQuantity_OnSuccess()
+        {
+            // Arrange
+            long price = 10;
+            int newQuant = 2;
+            int currQuant = 1;
+
+            long expecNewTotal = 20;
+            long expecCurrTotal = 10;
+
+            // Act
+            OrderItem item = new(Guid.NewGuid(), "Item 1", currQuant, price, Guid.NewGuid());
+            long currTotal = item.Total;
+
+            item.ChangeQuantity(newQuant);
+            long newTotal = item.Total;
+
+            // Assert
+            Assert.True(currTotal == expecCurrTotal);
+            Assert.True(newTotal == expecNewTotal);
+        }
+
+        [Fact]
+        public void CalculateTotal_ChangingPrice_OnSuccess()
+        {
+            // Arrange
+            int quantity = 2;
+            long newPrice = 20;
+            long currPrice = 10;
+
+            long expecNewTotal = 40;
+            long expecCurrTotal = 20;
+
+            // Act
+            OrderItem item = new(Guid.NewGuid(), "Item 1", quantity, currPrice, Guid.NewGuid());
+            long currTotal = item.Total;
+
+            item.ChangePrice(newPrice);
+            long newTotal = item.Total;
+
+            // Assert
+            Assert.True(currTotal == expecCurrTotal);
+            Assert.True(newTotal == expecNewTotal);
+        }
+    }
+}
