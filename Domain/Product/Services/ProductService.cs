5601c52a0c0938d030a518423439c358543444f3
HackbartPR <cgharckbart@gmail.com>
2024-01-23 21:14:02 -0300

    feature: Produt Domain Service - Increase Price

---
+++ Domain/Product/Services/ProductService.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Product.Services
+{
+    public class ProductService
+    {
+        public static void IncreasePrice(ICollection<Entity.Product> products, float porcentage)
+        {
+            products.ToList().ForEach(product => product.ChangePrice(product.Price * (1 + porcentage / 100)));
+        }
+    }
+}
---
+++ DomainUT/Product/Services/ProductServiceUT.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Product.Services;
+
+namespace DomainUT.Product.Services
+{
+    public class ProductServiceUT
+    {
+        [Fact]
+        public void IncreasePrice_Successfuly()
+        {
+            // Arrange
+            var product1 = new Domain.Product.Entity.Product(Guid.NewGuid(), "First", 10);
+            var product2 = new Domain.Product.Entity.Product(Guid.NewGuid(), "Second", 20);
+
+            var expected1 = 11;
+            var expected2 = 22;
+
+            ICollection<Domain.Product.Entity.Product> products = new List<Domain.Product.Entity.Product>{product1, product2};
+
+            // Act
+            ProductService.IncreasePrice(products, 10);
+
+            // Assert
+            Assert.True(product1.Price == expected1);
+            Assert.True(product2.Price == expected2);
+        }
+    }
+}
