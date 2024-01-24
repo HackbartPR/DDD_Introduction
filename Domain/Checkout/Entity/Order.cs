356b8e8cc097d33f68e3b4d3d0a81c6e9725770c
HackbartPR <cgharckbart@gmail.com>
2024-01-23 21:13:08 -0300

    feature: add Order

---
+++ Domain/Checkout/Entity/Order.cs
@@ -0,0 +1,42 @@
+﻿using Domain._Shared.Entity;
+
+namespace Domain.Checkout.Entity
+{
+    public class Order : BaseEntity
+    {
+        public Guid Id { get; private set; }
+        public long Total { get; private set; }
+        public Guid CustomerId { get; private set; }
+        public int RewardPoints { get; private set; }
+        public ICollection<OrderItem> Items { get; private set; }
+
+        public Order(Guid id, Guid customerId, int rewardPoints, ICollection<OrderItem> items)
+        {
+            Id = id;
+            Items = items;
+            Total = CalculateTotal();
+            CustomerId = customerId;
+            RewardPoints = rewardPoints;
+
+            Validate();
+        }
+
+        public override void Validate()
+        {
+            if (Id == Guid.Empty) throw new ArgumentException("Id is required!");
+            if (CustomerId == Guid.Empty) throw new ArgumentException("CustomerId is required!");
+            if (RewardPoints < 0) throw new ArgumentException("RewardPoints must be a positive number");
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+            CalculateTotal();
+        }
+
+        private long CalculateTotal()
+        {
+            return Items.Any() ? Items.Aggregate(0L, (result, item) => result + item.Total) : 0;
+        }
+    }
+}
---
+++ DomainUT/Checkout/Entity/OrderUT.cs
@@ -0,0 +1,117 @@
+﻿using Domain.Checkout.Entity;
+using Moq;
+
+namespace DomainUT.Checkout.Entity
+{
+    public class OrderUT
+    {
+        [Fact]
+        public void Create_Order_OnSuccess()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Guid customerId = Guid.NewGuid();
+
+            int rewardsPoints = 100;
+
+            ICollection<OrderItem> items = new List<OrderItem>();
+
+            // Act
+            Order order = new(id, customerId, rewardsPoints, items);
+
+            // Assert
+            Assert.True(order.Id == id);
+            Assert.True(order.CustomerId == customerId);
+            Assert.True(order.RewardPoints == rewardsPoints);
+            Assert.True(order.Total == 0);
+            Assert.True(order.Items == items);
+        }
+
+        [Fact]
+        public void Create_Order_InvalidId_OnFailed()
+        {
+            // Arrange
+            Guid id = Guid.Empty;
+            Guid customerId = Guid.NewGuid();
+
+            int rewardsPoints = 100;
+
+            ICollection<OrderItem> items = new List<OrderItem>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Order(id, customerId, rewardsPoints, items));
+        }
+
+        [Fact]
+        public void Create_Order_InvalidCustomerId_OnFailed()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Guid customerId = Guid.Empty;
+
+            int rewardsPoints = 100;
+
+            ICollection<OrderItem> items = new List<OrderItem>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Order(id, customerId, rewardsPoints, items));
+        }
+
+        [Fact]
+        public void Create_Order_InvalidRewardPoints_OnFailed()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Guid customerId = Guid.NewGuid();
+
+            int rewardsPoints = -100;
+
+            ICollection<OrderItem> items = new List<OrderItem>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Order(id, customerId, rewardsPoints, items));
+        }
+
+        [Fact]
+        public void AddItem_OnSuccess()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Guid customerId = Guid.NewGuid();
+
+            int rewardsPoints = 100;
+
+            Order order1 = new(id, customerId, rewardsPoints, new List<OrderItem>());
+            Order order2 = new(id, customerId, rewardsPoints, new List<OrderItem>());
+
+            // Act
+            order2.AddItem(new OrderItem(Guid.NewGuid(), "item1", 1, 10, Guid.NewGuid()));
+
+            // Assert
+            Assert.True(order1.Items.Count() == 0);
+            Assert.True(order2.Items.Count() == 1);
+        }
+
+        [Fact]
+        public void CalculateTotal_OnSuccess()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Guid customerId = Guid.NewGuid();
+
+            int rewardsPoints = 100;
+
+            ICollection<OrderItem> items = new List<OrderItem>
+            {
+                new OrderItem(Guid.NewGuid(), "item1", 1, 10, Guid.NewGuid()),
+                new OrderItem(Guid.NewGuid(), "item2", 2, 20, Guid.NewGuid()),
+            };
+
+            // Act
+            Order order = new(id, customerId, rewardsPoints, items);
+
+            // Assert
+            Assert.True(order.Total == 50);
+        }
+    }
+}
